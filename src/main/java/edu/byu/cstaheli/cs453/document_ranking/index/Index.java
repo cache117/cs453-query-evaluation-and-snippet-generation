914386b89fcdc36ad5f75ad6b978c2d27ae0d89a
Cache Staheli <cache@byu.edu>
2017-06-05 09:47:46 -0600

    Add additional project dependencies

---
+++ src/main/java/edu/byu/cstaheli/cs453/document_ranking/index/Index.java
@@ -0,0 +1,143 @@
+package edu.byu.cstaheli.cs453.document_ranking.index;
+
+import com.google.common.collect.Ordering;
+import com.google.common.collect.SortedSetMultimap;
+import com.google.common.collect.TreeMultimap;
+
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+/**
+ * Created by cstaheli on 5/12/2017.
+ */
+public class Index
+{
+    private static Index _instance;
+    //    private SortedSetMultimap<String, IndexEntry> entries;
+    private SortedSetMultimap<Integer, IndexEntry> entries;
+
+    private Index()
+    {
+        entries = TreeMultimap.create(Ordering.natural(), Ordering.natural().reversed());
+    }
+
+    public static Index getInstance()
+    {
+        if (_instance == null)
+        {
+            _instance = new Index();
+        }
+        return _instance;
+    }
+
+    public void addEntry(IndexEntry entry)
+    {
+        entries.put(entry.getDocumentId(), entry);
+    }
+
+    public SortedSet<Integer> getAllDocumentIds()
+    {
+        return (SortedSet<Integer>) entries.keySet();
+    }
+
+    public SortedSet<IndexEntry> getEntriesOfWord(String word)
+    {
+//        return entries.get(word);
+        return entries
+                .values()
+                .stream()
+                .filter(i -> word.equals(i.getWord()))
+                .collect(Collectors.toCollection(TreeSet<IndexEntry>::new));
+    }
+
+    public SortedSet<IndexEntry> getEntriesOfWordInDocument(String word, int documentId)
+    {
+        return getEntriesFromDocument(documentId)
+                .stream()
+                .filter(i -> word.equals(i.getWord()))
+                .collect(Collectors.toCollection(TreeSet<IndexEntry>::new));
+    }
+
+    public int getFrequencyOfWordInDocument(String word, int documentId)
+    {
+        return getEntriesOfWordInDocument(word, documentId)
+                .stream()
+                .mapToInt(IndexEntry::getFrequency)
+                .sum();
+    }
+
+    public int getNumberOfDocumentsWordIsPresentIn(String word)
+    {
+        return getDocumentIdsWhereWordPresent(word)
+                .size();
+    }
+
+    public SortedSet<IndexEntry> getEntriesFromDocument(int documentId)
+    {
+        return entries.get(documentId);
+    }
+
+//    public int size(String word)
+//    {
+//        return getEntriesOfWord(word).size();
+//    }
+
+    public int size()
+    {
+        return getAllDocumentIds().size();
+    }
+
+    public SortedSet<Integer> getDocumentIdsWhereWordPresent(String word)
+    {
+        return getEntriesOfWord(word)
+                .stream()
+                .map(IndexEntry::getDocumentId)
+                .collect(Collectors.toCollection(TreeSet<Integer>::new));
+    }
+
+//    public int totalFrequencyOfWord(String word)
+//    {
+//        return getEntriesOfWord(word)
+//                .stream()
+//                .mapToInt(IndexEntry::getFrequency)
+//                .sum();
+//    }
+
+    public int mostFrequentWordFrequency(int documentId)
+    {
+        return getEntriesOfWordInDocument(mostFrequentWord(documentId), documentId)
+                .stream()
+                .mapToInt(IndexEntry::getFrequency)
+                .sum();
+    }
+
+    public String mostFrequentWord(int documentId)
+    {
+        return entries
+                .get(documentId)
+                .first()
+                .getWord();
+    }
+
+//    /**
+//     * This method iterates through the values of the {@link TreeMultimap},
+//     * searching for {@link IndexEntry} objects which have their {@code word}
+//     * field equal to the parameter, word.
+//     *
+//     * @param word The word to search for in every document.
+//     * @return A {@link List<Pair<Integer, Integer>>} where each {@link Pair<>}
+//     * will hold the document's ID as its first element and the frequency
+//     * of the word in the document as its second element.
+//     * <p>
+//     * Note that the {@link Pair} object is defined in javafx.util.Pair
+//     */
+//    public List<Pair<Integer, Integer>> totalWordUses(String word)
+//    {
+//        return entries.values()
+//                .stream()
+//                .filter(i -> word.equals(i.getWord()))
+//                .map(i -> new Pair<>(i.getDocumentId(), i.getFrequency()))
+//                .collect(Collectors.toList());
+//    }
+}
---
+++ src/main/java/edu/byu/cstaheli/cs453/document_ranking/index/IndexEntry.java
@@ -0,0 +1,84 @@
+package edu.byu.cstaheli.cs453.document_ranking.index;
+
+/**
+ * Created by cstaheli on 5/12/2017.
+ */
+public class IndexEntry implements Comparable<IndexEntry>
+{
+    private final String word;
+    private final int frequency;
+    private final int documentId;
+
+    public IndexEntry(String word, int frequency, int documentId)
+    {
+        this.word = word;
+        this.frequency = frequency;
+        this.documentId = documentId;
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        IndexEntry that = (IndexEntry) o;
+
+        if (frequency != that.frequency) return false;
+        if (documentId != that.documentId) return false;
+        return word.equals(that.word);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        int result = word.hashCode();
+        result = 31 * result + frequency;
+        result = 31 * result + documentId;
+        return result;
+    }
+
+    @Override
+    public String toString()
+    {
+        return String.format("{%s:%s:%s}", word, documentId, frequency);
+    }
+
+    @Override
+    public int compareTo(IndexEntry other) {
+        int frequencyComparison = Integer.compare(this.frequency, other.frequency);
+        //overall ordering is flipped (Index uses reversed ordering for IndexEntry),
+        // so flip the word and document id comparisons
+        if (frequencyComparison == 0)
+        {
+            int wordComparison = other.word.compareTo(this.word);
+            if (wordComparison == 0)
+            {
+                return Integer.compare(other.getDocumentId(), this.documentId);
+            }
+            else
+            {
+                return wordComparison;
+            }
+        }
+        else
+        {
+            return frequencyComparison;
+        }
+    }
+
+    public String getWord()
+    {
+        return word;
+    }
+
+    public int getFrequency()
+    {
+        return frequency;
+    }
+
+    public int getDocumentId()
+    {
+        return documentId;
+    }
+}
